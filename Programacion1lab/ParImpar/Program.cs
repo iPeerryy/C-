6788f560672598b7fbd954bb8a4348686a568454
Jose Daniel Acosta Segura <josedanielacostasegura@gmail.com>
2025-02-16 19:19:09 -0400

    Added even or odd program

---
+++ Programacion1lab/ParImpar/Program.cs
@@ -0,0 +1,12 @@
+﻿int num1 = 0;
+
+Console.Write("Ingrese un numero: ");
+num1 = int.Parse(Console.ReadLine());
+if (num1 % 2 == 0)
+{
+    Console.WriteLine("El numero ingresado es par");
+}
+else
+{
+    Console.WriteLine("El numero ingresado es impar");
+}
