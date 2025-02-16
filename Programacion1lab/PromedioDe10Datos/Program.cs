007fea0ad5a8b988b76cad369fd87a83474eb8ee
Jose Daniel Acosta Segura <josedanielacostasegura@gmail.com>
2025-02-16 18:50:20 -0400

    Added average of 10 values program

---
+++ Programacion1lab/PromedioDe10Datos/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Timers;
+
+double[] valores = new double[10];
+double suma = 0;
+double Promedio5 = 0;
+for (int i = 0; i < 10; i++)
+{
+
+    Console.Write("Ingrese el valor numero " + (i+1) +": ");
+    valores[i] = double.Parse( Console.ReadLine() );
+
+    suma += valores[i];
+
+    if ( i == 4)
+    {
+         Promedio5 = suma / 5;
+    }
+}
+Console.WriteLine(" El promedio de los 5 primeros datos es de: " + Promedio5);
+Console.WriteLine("El promedio de los 10 valores es de: " + (suma / 10));
