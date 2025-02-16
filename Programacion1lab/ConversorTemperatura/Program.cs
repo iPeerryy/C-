08b193a1fdd802b087a2345959d7d39b80b67fc1
Jose Daniel Acosta Segura <josedanielacostasegura@gmail.com>
2025-02-16 19:09:06 -0400

    Added temperature conversor program

---
+++ Programacion1lab/ConversorTemperatura/Program.cs
@@ -0,0 +1,8 @@
+﻿double GradosC = 0;
+double GradosF = 0;
+Console.Write("Ingrese el la cantidad de grados Celsius que desea convertir a grados Fahrenheit: ");
+GradosC = double.Parse(Console.ReadLine());
+
+GradosF = (GradosC * 9 / 5) + 32;
+Console.WriteLine("El valor de grados Celsius en Fahrenheit es de: " + GradosF);
+
