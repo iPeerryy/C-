e7dd184592d8a65c05778bac35f8da64484f1165
Jose Daniel Acosta Segura <josedanielacostasegura@gmail.com>
2025-02-20 22:50:44 -0400

    Added corporal Body mass formula program

---
+++ Programacion1lab/Calculo de masa muscular/Program.cs
@@ -0,0 +1,11 @@
+﻿double PesoTotal, PorcentajeDeGrasaCorporal, PesoHuesos;
+
+Console.Write("Ingrese el pesos de los total: ");
+PesoTotal = double.Parse(Console.ReadLine());
+Console.Write("Ingrese el porcentaje de grasa corporal: ");
+PorcentajeDeGrasaCorporal = double.Parse(Console.ReadLine());
+PorcentajeDeGrasaCorporal = PorcentajeDeGrasaCorporal / 100;
+Console.WriteLine("Ingrese el pesos de los huesos y organos");
+PesoHuesos = double.Parse(Console.ReadLine());
+Console.WriteLine("La masa muscular del paciente es: " + (PesoTotal - (PesoTotal * PorcentajeDeGrasaCorporal) - PesoHuesos));
+
