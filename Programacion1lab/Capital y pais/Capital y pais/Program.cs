03325d3c3681ab6c0898d1da22c5560439242cb3
Jose Daniel Acosta Segura <josedanielacostasegura@gmail.com>
2025-02-28 11:45:51 -0400

    Added fin a city by texting its country program

---
+++ Programacion1lab/Capital y pais/Capital y pais/Program.cs
@@ -0,0 +1,38 @@
+﻿//Crea un diccionario donde las claves sean nombres de países y los valores sean capitales. Permite al usuario buscar la capital de un país ingresando el nombre del país.
+
+Dictionary<String, String> PaisCapital = new Dictionary<String, String>
+{
+	
+	{"Republica Dominicana", "Santo Domingo" },
+	{"Puerto Rico", "San Juan" },
+	{"Egipto", "El Cairo" },
+	{ "España", "Madrid" },
+	{ "Francia", "París" },
+	{ "Italia", "Roma" },
+	{ "Alemania", "Berlín" },
+	{ "Portugal", "Lisboa" },
+	{"Haiti", "Puerto Principe" }
+};
+Console.WriteLine("Ingrese el nombre de un pais para buscar su capital (O 'Salir' para terminar): ");
+
+while (true)
+{
+    Console.Write("Pais: ");
+	string Pais = Console.ReadLine();
+
+	if (Pais.ToLower() == "salir")
+	{
+		break;
+
+	}
+	if (PaisCapital.TryGetValue(Pais, out string Capital))
+	{
+        Console.WriteLine("La capital de "+ Pais + " es: " + Capital);
+	}
+	else
+	{
+        Console.WriteLine("No se encrontro ningun pais con el nombre ingresado ");
+	}
+}
+Console.WriteLine("gracias por utilizar el programa");
+
