54d5b8023efd917f0e0d7055654edf2880ac62bf
Jose Daniel Acosta Segura <josedanielacostasegura@gmail.com>
2025-02-28 10:06:16 -0400

    Added persona class program

---
+++ Programacion1lab/Conseguir datos de personas/Conseguir datos de personas/Program.cs
@@ -0,0 +1,30 @@
+﻿// Define una clase llamada Persona con propiedades como nombre, edad y género. Luego, crea instancias de esta clase y muestra la información de cada persona.
+
+Persona persona1 = new Persona("Juan", 32, "Masculino");
+Persona persona2 = new Persona("Daniel", 30, "Masculino");
+Persona persona3 = new Persona("Maria", 50, "Femenino");
+
+
+persona1.MostrarInformacion();
+persona2.MostrarInformacion();
+persona3.MostrarInformacion();
+class Persona
+{
+	string Nombre { get; set; }
+	int Edad { get; set; }
+	string Genero { get; set; }
+
+	public Persona (string nombre, int edad, string genero)
+	{
+		Nombre = nombre;
+		this.Edad = edad;
+		this.Genero = genero;
+	}
+	public void MostrarInformacion()
+	{
+		Console.WriteLine("Nombre: " + Nombre + ", Edad: " + Edad + ", Genero: " + Genero);
+
+
+	}
+}
+
