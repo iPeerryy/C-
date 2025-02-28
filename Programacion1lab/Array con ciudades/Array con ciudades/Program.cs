f671ac2bba690c57766e3bb8378b0fa44ad32ec2
Jose Daniel Acosta Segura <josedanielacostasegura@gmail.com>
2025-02-28 11:29:36 -0400

    Added  cities program

---
+++ Programacion1lab/Array con ciudades/Array con ciudades/Program.cs
@@ -0,0 +1,21 @@
+﻿List<String> ciudades = new List<string> { "Madrid", "Santiago", "Santo Domingo", "New Yersey" };
+
+Console.WriteLine("Ingrese nombre de ciudades. Escriba 'Fin' para terminar" );
+
+while (true)
+{
+    Console.Write("Ciudad: ");
+    string ciudad = Console.ReadLine();
+
+    if (ciudad.ToLower() == "fin")
+    {
+        break;
+    }
+    ciudades.Add(ciudad);
+}
+
+Console.WriteLine("Ciudades Ingresadas: ");
+foreach (string ciudad in ciudades)
+{
+	Console.WriteLine(ciudad);
+}
